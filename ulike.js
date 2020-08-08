5cca03b94f136c7e666f161c7564ea7eb24a732b
htwind <62308666+htwind@users.noreply.github.com>
2020-08-08 12:33:21 +0700

    Update ulike.js

--- ulike.js
+++ ulike.js
@@ -1,20 +1,18 @@
 var obj = JSON.parse($response.body);
 
 obj= {
-  "ret": "0",
-  "errmsg": "success",
-  "systime": "1596773561565",
   "data": {
-    "flag": true,
-    "start_time": 1596095422,
     "end_time": 1596700237,
+    "uid": "0",
+    "start_time": 1596095422,
+    "flag": true,
     "is_first_subscribe": true,
     "is_cancel_subscribe": true,
-    "uid": "0",
-    "subscribe_uid": "6855166664344110593",
-    "subscribe_type": "auto",
-    "authkey": "a36411db5e0c8a54946902fa86599a5f"
-  }
+    "subscribe_type": "auto"
+  },
+  "systime": "",
+  "errmsg": "success",
+  "ret": "0"
 };
 
 $done({body: JSON.stringify(obj)});

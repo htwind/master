7aaf296e1d9928c3a7f9c76d8d53447bcce01418
htwind <62308666+htwind@users.noreply.github.com>
2020-08-15 01:09:02 +0700

    Update Strucc.js

--- Strucc.js
+++ Strucc.js
@@ -12,25 +12,18 @@ obj= {
     },
     "first_seen": "2020-08-11T17:47:47Z",
     "last_seen": "2020-08-11T17:57:01Z",
-    "management_url": "itms-apps://apps.apple.com/account/subscriptions",
-    "non_subscriptions": {},
-    "original_app_user_id": "$RCAnonymousID:4f472d5591974fe294d2d5b3cff0de63",
-    "original_application_version": "792",
     "original_purchase_date": "2020-08-11T17:37:53Z",
     "other_purchases": {},
     "subscriptions": {
       "studio.sweet.Strucc.yearly": {
-        "billing_issues_detected_at": null,
         "expires_date": "2021-08-11T17:54:14Z",
         "is_sandbox": false,
         "original_purchase_date": "2020-08-11T17:54:14Z",
         "period_type": "normal",
-        "purchase_date": "2020-08-11T17:54:14Z",
-        "store": "app_store",
-        "unsubscribe_detected_at": "2020-08-11T17:58:19Z"
+        "purchase_date": "2020-08-11T17:54:14Z"
       }
     }
   }
 };
 
-$done({body: JSON.stringify(obj)});
+$done({body: JSON.stringify(obj)}); 

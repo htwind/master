8b52ef2c76fc617b1bed7fcd7813807477034966
htwind <62308666+htwind@users.noreply.github.com>
2020-08-19 19:05:38 +0700

    Update Ewasub.js

--- Ewasub.js
+++ Ewasub.js
@@ -2,11 +2,13 @@ var obj = JSON.parse($response.body);
 
 obj= {
   "result": {
-    "_id": "",
+    "_id": "1864ca6d-3800-4f33-a3b8-510d95319cbe",
+    "ownerId": null,
     "subscriptionId": premium,
+    "permissions": null,
     "createDate": "2020-08-18T16:40:07+0000",
     "startDate": "2020-08-18T16:40:04+0000",
     "endDate": "2099-08-22T16:40:04+0000"
-  }
+  };
 
  $done({body: JSON.stringify(obj)});

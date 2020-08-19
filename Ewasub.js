ab7f74cf34869c8e4d6da1a5b189bcfd24a8dc52
htwind <62308666+htwind@users.noreply.github.com>
2020-08-19 19:06:49 +0700

    Update Ewasub.js

--- Ewasub.js
+++ Ewasub.js
@@ -9,6 +9,7 @@ obj= {
     "createDate": "2020-08-18T16:40:07+0000",
     "startDate": "2020-08-18T16:40:04+0000",
     "endDate": "2099-08-22T16:40:04+0000"
-  };
+  }
+};
 
  $done({body: JSON.stringify(obj)});

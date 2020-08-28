e4f6f5d2b471266934ec4a4192020468e4156364
htwind <62308666+htwind@users.noreply.github.com>
2020-08-28 22:14:16 +0700

    Create AM.js

---
+++ AM.js
@@ -0,0 +1,37 @@
+var obj = JSON.parse($response.body);
+
+obj= {
+  "result" : {
+    "result" : "success",
+    "msTime" : 1598627381780,
+    "accountCreatedMillis" : 1598626456000,
+    "licenses" : [
+      {
+        "period" : "1y",
+        "orderNumber" : "460000648719327",
+        "benefits" : [
+          "RemoveWatermark",
+          "MemberEffects",
+          "ProjectPackageSharing",
+          "FutureMemberFeatures",
+          "AdvancedEasing"
+        ],
+        "autoRenewing" : true,
+        "productId" : "com.alightcreative.motion.sub.yearly",
+        "details" : null,
+        "valid" : true,
+        "label" : null,
+        "type" : "subscription",
+        "expires" : 9999999999999,
+        "store" : "apple_app_store"
+      }
+    ],
+    "warnings" : [
+
+    ]
+  }
+};
+
+$done({body: JSON.stringify(obj)});
+
+//HT

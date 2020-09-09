0ec166f1fb9fa4c346280d4e204a864bb6dc3491
htwind <62308666+htwind@users.noreply.github.com>
2020-09-09 12:41:33 +0700

    Create LFC.js

---
+++ LFC.js
@@ -0,0 +1,22 @@
+var obj = JSON.parse($response.body);
+obj= {
+  "result": "ok",
+  "message": "",
+  "user_info": {
+    "user_phone": "",
+    "user_email": "ltmoffline@gmail.com",
+    "user_birth": "",
+    "user_sex": " ",
+    "user_name": "",
+    "user_nickname": "Wind95"
+  },
+  "list": [],
+  "access_token": "bb6896f7921b99ad9662273744a729a2fa7a7d80",
+  "payment_status": "1",
+  "code": "200"
+};
+
+$done({body: JSON.stringify(obj)});
+
+
+//HT

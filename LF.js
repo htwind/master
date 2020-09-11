41907bb72b7ccafd7a4338ca54cc9628a3c80487
htwind <62308666+htwind@users.noreply.github.com>
2020-09-11 17:30:53 +0700

    Update LF.js

--- LF.js
+++ LF.js
@@ -1,14 +1,6 @@
 var obj = JSON.parse($response.body);
 
 obj= {
-  "registerdate": "2020-09-01",
-  "startdate": "2020-09-01",
-  "usesmetric": true,
-  "useskj": false,
-  "usesstones": false,
-  "activity": 1.375,
-  "measurement_type": "eu",
-  "haspassword": true,
   "premium": true
 };
 

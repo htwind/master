01d595afb70eb8bb383d1fd136980bb23d02444b
htwind <62308666+htwind@users.noreply.github.com>
2020-10-05 16:47:00 +0700

    Create Sp.js

---
+++ Sp.js
@@ -0,0 +1,26 @@
+var obj = JSON.parse($response.body);
+obj= {
+
+{
+  "entitlements": [
+    {
+      "entitlement_id": "premium",
+      "expires_date_ms": 9999999999999,
+      "purchase_date_ms": 1601890378000,
+      "product_identifier": "SpeedTest_Lifetime_Premium_20190920",
+      "is_in_trial_period": false,
+      "is_in_intro_offer_period": false,
+      "environment": "Production",
+      "redeem": {},
+      "auto_renew": true
+    }
+  ],
+  "is_valid": true
+};
+
+
+
+$done({body: JSON.stringify(obj)});
+
+
+//HT
